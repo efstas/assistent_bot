3717e66bbc424fd3587216726fb40b8c4268e1dc
efstas <efstas@yandex.ru>
2023-11-08 09:18:38 +0100

    Removed

--- main.py
+++ main.py
@@ -15,10 +15,41 @@ dp = Dispatcher(bot)
 
 date_object = datetime.date.today()
 
+def is_admin(user_id):
+    return str(user_id) == "****"  # Возвращает True, если пользователь имеет ID ****, иначе False
+
+@dp.message_handler(commands=["bot_log"])
+async def get_bot_log(message: types.Message):
+    if is_admin(message.from_user.id):
+        try:
+            with open("bot.log", "r") as log_file:
+                log_text = log_file.read()
+            await bot.send_message(message.from_user.id, log_text)
+        except FileNotFoundError:
+            await bot.send_message(message.from_user.id, "Файл bot.log не найден.")
+    else:
+        # If the user is not an administrator
+        await message.answer("Вы не являетесь администратором и не имеете доступа к этой команде.")
+
+
+def read_stop_words():
+    try:
+        with open("stop_words.txt", "r") as file:
+            words = [word.strip() for word in file.readlines()]
+    except FileNotFoundError:
+        words = []
+    return words
+
+
+def load_stop_words():
+    with open("stop_words.txt", "r") as file:
+        return [line.strip() for line in file]
+
 
 @dp.message_handler(commands=["start"])
 async def start(message: types.Message):
     await message.answer("Привет! Я бот. Если есть вопросы пиши создателю - @efstas.")
+
 
 @dp.message_handler(commands=["user_id"])
 async def send_message(message: types.Message):
@@ -27,8 +58,25 @@ async def send_message(message: types.Message):
 
 @dp.message_handler(commands=["stop_words"])
 async def stopwords(message: types.Message):
-    words_list = "\n".join(cfg.WORDS)
-    await bot.send_message(message.from_user.id, f'В данный момент я удаляю слова:\n\n{words_list}')
+    if is_admin(message.from_user.id):
+        stop_words_list = "\n".join(load_stop_words())
+        await bot.send_message(message.from_user.id, f'В данный момент я удаляю слова:\n\n{stop_words_list}')
+    else:
+        await message.answer("Вы не являетесь администратором и не имеете доступа к этой команде.")
+
+
+@dp.message_handler(commands=["add_word"])
+async def add_word(message: types.Message):
+    if is_admin(message.from_user.id):
+        text = message.get_args()
+        if text:
+            with open("stop_words.txt", "a") as file:
+                file.write(text + "\n")
+            await message.answer(f'Слово "{text}" успешно добавлено в список stop_words.')
+        else:
+            await message.answer("Вы не указали слово для добавления в список stop_words.")
+    else:
+        await message.answer("Вы не являетесь администратором и не имеете доступа к этой команде.")
 
 
 def check_chat_member(chat_member):
@@ -52,16 +100,16 @@ async def user_joined(message: types.Message):
         await asyncio.sleep(60)
         await msg.delete()
 
-
 @dp.message_handler()
 async def mess_handler(message: types.Message):
+    stop_words = load_stop_words()
     if check_chat_member(await bot.get_chat_member(chat_id=cfg.CHANNEL_ID, user_id=message.from_user.id)):
-        for word in cfg.WORDS:
+        for word in stop_words:
             if word in message.text:
-                await bot.send_message(chat_id=241943423, text=f'❗ Дата нарушения - {date_object}\n\n'
-                                                           f'Ссылка на нарушителя: @{message.from_user.username}\n'
-                                                           f'ID нарушителя: ID{message.from_user.id}\n')
-                await message.forward(chat_id=241943423, disable_notification=True)
+                await bot.send_message(chat_id=cfg.ADMIN_ID, text=f'❗ Дата нарушения - {date_object}\n\n'
+                                                                  f'Ссылка на нарушителя: @{message.from_user.username}\n'
+                                                                  f'ID нарушителя: ID{message.from_user.id}\n')
+                await message.forward(chat_id=cfg.ADMIN_ID, disable_notification=True)
                 await message.delete()
                 message_stopwords = await message.answer(f'👮🏼♂️ Пришлось удалить сообщение, там были плохие слова...')
                 await asyncio.sleep(120)
@@ -76,10 +124,15 @@ async def mess_handler(message: types.Message):
                                                       "У тебя есть одна минута, потом проверим :)\n\n"
                                                       "Присоединяйся: youtube.com/esblog  & instagram.com/es_blogde \n\n",
                                                       reply_markup=nav.channelMenu)
-        await asyncio.sleep(60)
-        await message_no_chat_member.delete()
+
+        try:
+            await asyncio.sleep(60)
+            await message_no_chat_member.delete()
+        except aiogram.utils.exceptions.MessageToDeleteNotFound:
+            pass
 
 
 if __name__ == "__main__":
-    print("bot go go go")
+    print("Let the work shift begin!")
     executor.start_polling(dp, skip_updates=True)
+
